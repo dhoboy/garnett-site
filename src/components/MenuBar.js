a32e529e4a81553133eaad7d086b122152835fea
Daniel Overstreet <overstreet@collectivedigitalstudio.com>
2020-12-31 02:29:08 +0900

    Style changes

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -6,7 +6,7 @@ const MenuBar = () => {
     <ul className="menu-bar">
       <MenuBarItem display="News" path="/news" />
       <MenuBarItem display="Bio" path="/bio" />
-      <MenuBarItem display="How I Teach" path="/how-i-teach" />
+      <MenuBarItem display="How I Work" path="/how-i-teach" />
       <MenuBarItem display="ACAC" path="/acac" />
       <MenuBarItem display="Testimonial" path="/testimonials" />
       <MenuBarItem display="Contact" path="/contact" />
