9cccc2b284ebb33ba032991ecc78e771d695d2ca
Daniel Overstreet <overstreet@collectivedigitalstudio.com>
2021-02-08 20:31:43 +0900

    Updates from feedback

--- src/components/HowTeach.js
+++ src/components/HowTeach.js
@@ -5,28 +5,38 @@ const HowTeach = () => {
     <div className="how-teach">
       <p className="blurb">
         <span>Intro</span>
-        Do you want to move more and at the same time feel confident and easy? 
-        You’ll benefit from my sessions which are a synthesis of all the movement disciplines I've trained in.
+        Do you strive to be more active physically and feel confident in your ability to move different parts of your body safely, smoothly and efficiently?  
+        If so, you will benefit from my work, which is a synthesis of multiple disciplines, including Alexander Technique, personal training, Pilates, Yoga, Qi Gong, and even waltz. 
       </p>
       <p className="blurb">
-        Alexander work is my foundation. I became an AmSAT certified Alexander Technique teacher here at the Alexander Technique Training Center in Charlottesville in 2016.
-        I also work as a personal trainer through ACAC, and practice Qi Gong and Waltz.
+        The Alexander practice is my foundation. I became an AmSAT certified Alexander Technique teacher at the Alexander Technique Training Center in Charlottesville in 2012. 
+        I blend in the other disciplines to suit individual client needs and desires to explore exercising safely and with confidence.
       </p>
       <p className="blurb">
-        <span>How to schedule with me</span>
-        words will go here
+        You can take a series Alexander Technique lessons, explore personal training, or both.
       </p>
       <p className="blurb">
-        <span>How long are sessions?</span>
-        words will go here
+        I teach online using one of the currently available meeting platforms, and also in-person, observing the appropriate COVID safety precautions.
       </p>
       <p className="blurb">
-        <span>My rate</span>
-        words will go here
+        Through an initial discussion, we will talk about which type of services would serve you best, what length sessions to schedule, and where to meet.
+      </p>
+      <p className="blurb">
+        <span>When you are ready to have this discussion:</span>
+        Contact me at garnettmellen@gmail.com or call my work cell phone at 434-260-7794.
+      </p>
+      <p className="blurb">
+        <span>How long are lessons/sessions?</span>
+        Alexander Technique lessons are usually 45 minutes. 
+        The personal training sessions through ACAC are either 30 or 60 minute. 
+      </p>
+      <p className="blurb">
+        <span>Package rates?</span>
+        The costs vary depending on how many times you would like to meet. I can send a rate sheet upon request. 
       </p>
       <p className="blurb">
         <span>A Brief Note</span>
-        what they need to know, what will happen. what to wear, etc...
+        To begin working together, there is a very short questionnaire I will ask you to complete prior to our first meeting. I will also ask you about your current level of health so that I can customize the program to your needs.
       </p>
     </div>
   );
