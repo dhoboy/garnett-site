143d5444c5cab2149e225c283492cce20f58a68e
Daniel Overstreet <overstreet@collectivedigitalstudio.com>
2021-02-08 20:42:04 +0900

    Updates

---
+++ precache-manifest.44515228e8748524a173118b714dc85a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d01d9dd30e5253de584bce61133f68f",
+    "url": "/garnett-site/index.html"
+  },
+  {
+    "revision": "f40ec97d411d9b68b80b",
+    "url": "/garnett-site/static/css/main.4ab60b96.chunk.css"
+  },
+  {
+    "revision": "c8843e731e102d08055f",
+    "url": "/garnett-site/static/js/2.b66c24b3.chunk.js"
+  },
+  {
+    "revision": "870267bc23303939d9ca31cdf878e7b3",
+    "url": "/garnett-site/static/js/2.b66c24b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f40ec97d411d9b68b80b",
+    "url": "/garnett-site/static/js/main.b0a59fb4.chunk.js"
+  },
+  {
+    "revision": "3ea7ff297a1ed21510f3",
+    "url": "/garnett-site/static/js/runtime-main.5a279c2d.js"
+  },
+  {
+    "revision": "1a4f65bb13ed15463f8188d73fcad3ca",
+    "url": "/garnett-site/static/media/garnett.1a4f65bb.jpg"
+  }
+]);
